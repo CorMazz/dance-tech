5f43f7e09cf13ff58aa389726002f15a0dfcf67c
Corrado Mazzarelli <corrado@mazzarelli.biz>
2025-06-07 12:40:44 -0400

    Refactor router
    Split the router parts to reduce the likelihood of developer error
    exposing an otherwise private route.

--- src/app/router.rs
+++ src/app/router.rs
@@ -15,24 +15,40 @@ use crate::{
 use tower_http::services::ServeDir;
 
 pub fn create_router(app_state: Arc<AppState>) -> Router {
+
+    // Anything in here will redirect to the login page if the user is not logged in
+    let auth_required = Router::new()
+        .route("/logout", get(get_logout_page));
+
+    // Anything in here will check if the user is signed in and add that info to the request
+    let check_auth = Router::new()
+        .route("/", get(get_home_page))
+        .route("/sign-up", get(get_signup_page).post(post_signup_form))
+        .route("/login", get(get_login_page).post(post_login_form))
+        .route("/private/user-dropdown", get(get_user_dropdown));
+
+    // Anything in here does not require authentication
+    let no_auth = Router::new()
+        .route("/auth/google", get(get_google_oauth_init_flow))
+        .route("/auth/google/callback", get(get_google_oauth_callback));
+
+    // Do not edit this unless necessary. Add routes to the router subsections above this
     Router::new()
-        .route("/logout", get(get_logout_page))
+        .merge(auth_required) 
         .route_layer(middleware::from_fn_with_state(
             app_state.clone(),
             require_auth_middleware,
         ))
+
         // Anything above this line will redirect to the login page if the user is not logged in
-        .route("/", get(get_home_page))
-        .route("/sign-up", get(get_signup_page).post(post_signup_form))
-        .route("/login", get(get_login_page).post(post_login_form))
-        .route("/private/user-dropdown", get(get_user_dropdown))
+        .merge(check_auth)
         .route_layer(middleware::from_fn_with_state(
             app_state.clone(),
             check_auth_middleware,
         ))
+        
         // Anything above this line checks if the user is logged in and adds an AuthStatus extension to the request
-        .route("/auth/google", get(get_google_oauth_init_flow))
-        .route("/auth/google/callback", get(get_google_oauth_callback))
+        .merge(no_auth)
         .with_state(app_state)
         .nest_service("/static", ServeDir::new("static/"))
 }
