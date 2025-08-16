9259195105f9d41bb8d3bfb17cc01b4ab645807d
Corrado Mazzarelli <corrado@mazzarelli.biz>
2025-08-16 10:29:07 -0400

    Remove CORS
    We don't have any cross-origin requests, so remove that layer.

--- src/main.rs
+++ src/main.rs
@@ -111,12 +111,6 @@ async fn main() {
         }
     };
 
-    let cors = CorsLayer::new()
-        .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
-        .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::DELETE])
-        .allow_credentials(true)
-        .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
-
     let app_state = Arc::new(AppState {
         db: pool,
         app_config: app_config.clone(),
@@ -135,7 +129,6 @@ async fn main() {
     });
 
     let app = create_router(app_state)
-        .layer(cors)
         .layer(TraceLayer::new_for_http());
 
     info!(
